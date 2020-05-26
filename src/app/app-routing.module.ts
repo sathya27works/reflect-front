7fc9e173c30e884cf1b96b7b55aa89d2635192d4
unknown <ctsjavauser179@iiht.tech>
2020-05-26 21:28:00 +0530

    Curiosity Base page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
         component: HomePageReflectComponent
       },
       {
-        path: 'curiosityQuizQuery',
+        path: 'curiosityQuiz',
         component: CuriosityQuizQueryComponent
       }
     ]
